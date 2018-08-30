f35e366c9ea7c7e0b8255b5ebafaaad5f6a33549
fzhi <zhi.tianmu@gmail.com>
2018-08-30 16:09:13 -0400

    first

--- test.py
+++ test.py
@@ -7,14 +7,14 @@ test_key = '7C973F6B-9E95-49DA-8E9E-55F35FC3092F'
 
 api = CoinAPIv1(test_key)
 exchanges = api.metadata_list_exchanges()
-start = datetime.datetime(2018, 7, 26, 7, 6, 9, 0).isoformat()
-end = datetime.datetime(2018, 7, 27, 0, 0, 0, 0).isoformat()
+start = datetime.datetime(2018, 7, 26, 15, 1, 7, 0).isoformat()
+end = datetime.datetime(2018, 7, 28, 0, 0, 0, 0).isoformat()
 
 historical_trades_eth = api.trades_historical_data('COINBASE_SPOT_ETH_USD', {'time_start': start, 'time_end': end, 'limit': 10000})
 
 historical_trades_eth = pd.DataFrame(historical_trades_eth)
 
-writer = pd.ExcelWriter('eth_dataset_07_26_2018_2.xlsx')
+writer = pd.ExcelWriter('eth_dataset_07_26_2018_3.xlsx')
 historical_trades_eth.to_excel(writer, 'set1')
 writer.save()
 
